00b6302e4619b85252fa7f9f08249e9e2aff5179
Nguyễn Đình Thái <dinhthai0312@gmail.com>
2025-12-07 22:36:10 +0700

    first commit

--- lawchater_be/settings.py
+++ lawchater_be/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -80,21 +80,27 @@ WSGI_APPLICATION = 'lawchater_be.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-
+import os
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": os.getenv("DB_NAME", "lawchat"),
-        "USER": os.getenv("DB_USER", "django"),
-        "PASSWORD": os.getenv("DB_PASSWORD", "django123"),
-        "HOST": os.getenv("DB_HOST", "127.0.0.1"),  # trong docker sẽ là 'mysql'
-        "PORT": os.getenv("DB_PORT", "3306"),
-        "OPTIONS": {
-            "charset": "utf8mb4",
-        },
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": os.getenv("DB_NAME", "lawchat"),
+#         "USER": os.getenv("DB_USER", "django"),
+#         "PASSWORD": os.getenv("DB_PASSWORD", "django123"),
+#         "HOST": os.getenv("DB_HOST", "127.0.0.1"),  # trong docker sẽ là 'mysql'
+#         "PORT": os.getenv("DB_PORT", "3306"),
+#         "OPTIONS": {
+#             "charset": "utf8mb4",
+#         },
+#     }
+# }
+
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
