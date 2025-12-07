9d7ca9825c1eb9e917e3a5e791ec77c233963304
Nguyễn Đình Thái <dinhthai0312@gmail.com>
2025-12-07 22:39:27 +0700

    ss

--- lawchater_be/settings.py
+++ lawchater_be/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=_!spb#^^$+hhkp-k(aebmor&s709i@02vedp*n!ir2@*5_z1d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
